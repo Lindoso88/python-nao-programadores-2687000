8586079094e5d2e59ec059cd0a5ba56e1d10cbbd
Lindoso88 <153727032+Lindoso88@users.noreply.github.com>
2024-03-09 14:53:04 +0000

    resolução de exercício

--- exercicios/1-primeiros-passos/2-string.py
+++ exercicios/1-primeiros-passos/2-string.py
@@ -1,25 +1,21 @@
 resumo = "Paloma é uma mulher de 46 anos que deseja mudar de profissão, por isso está estudando 'python'."
 
 # Imprima na tela a variável "resumo"
-
+print(resumo)
 
 # Imprima na tela apenas a segunda letra da variável
-
-
+resumo[1]
 # Imprima na tela a idade de Paloma (resposta esperada: "46")
-
-
+idade = 46 
+print(idade)
 # Imprima na tela o trecho final da variável
-
-
+resumo[35:]
 # Converta todos as letras para minúsculo e imprima na tela
-
-
+resumo.lower()
 # Converta todas as letras para maiúscula e imprima na tela
-
-
+resumo.upper()
 # Formate a frase para que a primeira letra de cada palavra seja maiúscula e imprima na tela
-
+resumo.title()
 
 # Formate a frase para que apenas a primeira letra da frase seja maiúscula e imprima na tela
 
