220207276bc62483a84e3180f99b5de50039f944
Lindoso88 <153727032+Lindoso88@users.noreply.github.com>
2024-03-09 16:38:25 +0000

    resolução de exercício

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,16 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
+tipo_dn = type(data_nascimento)
+tipo_in = type(idade_numerica)
+tipo_a = type(altura)
 
+print(tipo_dn)
+print(tipo_in)
+print(tipo_a)
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+print('05-10-1976'*5)
 
 # Realize uma operação entre dados do tipo inteiro e float
+print(2*2.06)
